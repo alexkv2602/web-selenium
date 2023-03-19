6a4dcded39826567f1c3dbad9dab41f920116a3a
AlexKV <bloodklesh@gmail.com>
2023-03-20 02:36:42 +0300

    code added

--- src/main/java/ru/netoligy/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.netoligy;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/test/java/ru/netoligy/web/CallbackTest.java
@@ -0,0 +1,48 @@
+package ru.netoligy.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CallbackTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void shouldOpenPage() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Джордж-Майкл Корлеоне");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79996667788");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
+        assertEquals(expected, actual);
+
+
+    }
+
+}
